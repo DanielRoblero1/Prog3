5e393c7dc3cf126f3af65f75069a69380d5c5568
Daniel_Roblero <eroblerosc@miumg.edu.gt>
2022-02-03 23:22:34 -0600

    Agregacion tablas usuario y usuarioperfil

--- scripts/Script Prueba.sql
+++ scripts/Script Prueba.sql
@@ -1,27 +1,157 @@
-DROP DATABASE prueba;
-CREATE DATABASE prueba;
-USE prueba;
-CREATE TABLE usuario(
-	usucodigo VARCHAR(15) PRIMARY KEY,
-	usunombre varchar(60) NOT NULL, 
-	usudireccion VARCHAR(60) NOT NULL,
-    usutelefono VARCHAR(30) NOT NULL,
-    usucorreo VARCHAR(60) NOT NULL,
-	usucontra VARCHAR(100) NOT NULL,
-	usuestado VARCHAR(1) NOT NULL	
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-CREATE TABLE aplicaciones(
-	aplcodigo VARCHAR(5) PRIMARY KEY,
-	aplnombre varchar(60) NOT NULL, 
-	aplestado VARCHAR(10) NOT NULL
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-02-2022 a las 06:21:19
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `prueba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `aplicaciones`
+--
+
+CREATE TABLE `aplicaciones` (
+  `aplcodigo` varchar(5) NOT NULL,
+  `aplnombre` varchar(60) NOT NULL,
+  `aplestado` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE bitacora (
-	bitcodigo int auto_increment PRIMARY KEY,
-    bitfecha datetime NULL, 
-	bitaccion VARCHAR(10) NOT NULL,
-    usucodigo VARCHAR(15) NOT NULL,
-    aplcodigo VARCHAR(5) NOT NULL,
-	FOREIGN KEY (usucodigo) REFERENCES usuario (usucodigo),
-    FOREIGN KEY (aplcodigo) REFERENCES aplicaciones (aplcodigo)
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bitacora`
+--
+
+CREATE TABLE `bitacora` (
+  `bitcodigo` int(11) NOT NULL,
+  `bitfecha` datetime DEFAULT NULL,
+  `bitaccion` varchar(10) NOT NULL,
+  `usucodigo` varchar(15) NOT NULL,
+  `aplcodigo` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perfil`
+--
+
+CREATE TABLE `perfil` (
+  `perfid` varchar(40) NOT NULL,
+  `perfnombre` varchar(60) NOT NULL,
+  `perfestatus` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `usucodigo` varchar(15) NOT NULL,
+  `usunombre` varchar(60) NOT NULL,
+  `usudireccion` varchar(60) NOT NULL,
+  `usutelefono` varchar(30) NOT NULL,
+  `usucorreo` varchar(60) NOT NULL,
+  `usucontra` varchar(100) NOT NULL,
+  `usuestado` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarioperfil`
+--
+
+CREATE TABLE `usuarioperfil` (
+  `perfid` varchar(40) NOT NULL,
+  `usucodigo` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `aplicaciones`
+--
+ALTER TABLE `aplicaciones`
+  ADD PRIMARY KEY (`aplcodigo`);
+
+--
+-- Indices de la tabla `bitacora`
+--
+ALTER TABLE `bitacora`
+  ADD PRIMARY KEY (`bitcodigo`),
+  ADD KEY `usucodigo` (`usucodigo`),
+  ADD KEY `aplcodigo` (`aplcodigo`);
+
+--
+-- Indices de la tabla `perfil`
+--
+ALTER TABLE `perfil`
+  ADD PRIMARY KEY (`perfid`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`usucodigo`);
+
+--
+-- Indices de la tabla `usuarioperfil`
+--
+ALTER TABLE `usuarioperfil`
+  ADD KEY `perfid` (`perfid`),
+  ADD KEY `usucodigo` (`usucodigo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `bitacora`
+--
+ALTER TABLE `bitacora`
+  MODIFY `bitcodigo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `bitacora`
+--
+ALTER TABLE `bitacora`
+  ADD CONSTRAINT `bitacora_ibfk_1` FOREIGN KEY (`usucodigo`) REFERENCES `usuario` (`usucodigo`),
+  ADD CONSTRAINT `bitacora_ibfk_2` FOREIGN KEY (`aplcodigo`) REFERENCES `aplicaciones` (`aplcodigo`);
+
+--
+-- Filtros para la tabla `usuarioperfil`
+--
+ALTER TABLE `usuarioperfil`
+  ADD CONSTRAINT `usuarioperfil_ibfk_1` FOREIGN KEY (`perfid`) REFERENCES `perfil` (`perfid`),
+  ADD CONSTRAINT `usuarioperfil_ibfk_2` FOREIGN KEY (`usucodigo`) REFERENCES `usuario` (`usucodigo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
